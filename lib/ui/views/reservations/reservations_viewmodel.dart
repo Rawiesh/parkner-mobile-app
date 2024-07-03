67af60112ce2b1bcca14f74c647c6836fcd2aff1
Rawiesh <rawieshbehari@gmail.com>
2024-07-03 16:47:13 -0300

    feat(reservation): get reservation data from api

--- lib/ui/views/reservations/reservations_viewmodel.dart
+++ lib/ui/views/reservations/reservations_viewmodel.dart
@@ -1,5 +1,6 @@
-import 'dart:developer';
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 import 'package:parkner_mobile_app/app/app.locator.dart';
 import 'package:parkner_mobile_app/app/app.router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -9,12 +10,24 @@ import 'package:stacked_services/stacked_services.dart';
 class ReservationsViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
   SharedPreferences? prefs;
-  List<String> reservations = [];
+  List<String> reservationIds = [];
+  List<dynamic> reservations = [];
 
   Future<void> initialiseVM() async {
     prefs = await SharedPreferences.getInstance();
-    reservations = prefs?.getStringList('reservations') ?? [];
-    inspect(reservations);
+    reservationIds = prefs?.getStringList('reservations') ?? [];
+    for (var id in reservationIds) {
+      await updateReservationsList(id);
+    }
+  }
+
+  Future<void> updateReservationsList(String id) async {
+    final response = await http
+        .get(Uri.parse("https://parkner.vercel.app/api/lots/reservations/$id"));
+    if (response.statusCode == 200) {
+      var reservationData = jsonDecode(response.body);
+      reservations.add(reservationData);
+    }
   }
 
   void navigateToHome() {
